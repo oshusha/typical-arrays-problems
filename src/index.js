741edaed6a2e56df5b960a486d747f95b6b03714
Anastasia Oshurkova <adoshurkova@gmail.com>
2020-03-18 22:34:17 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!arguments.length || !array.length) {
+        return 0;
+    }
+    return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!arguments.length || !array.length) {
+        return 0;
+    }
+    return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(!arguments.length || !array.length){
+        return 0
+    }
+
+    return array.reduce((total, amount, index, array) => {
+        total += amount;
+        if( index === array.length - 1 ) {
+            return total/array.length;
+        } else {
+            return total;
+        }
+    });
 }
